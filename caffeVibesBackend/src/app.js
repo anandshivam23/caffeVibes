feb291248ef1d59a0cdc43647fda324b4d15e59e
anandshivam23 <anandshivam545@gmail.com>
2026-08-07 10:43:13 +0530

    fixing version issues in express // path error

--- caffeVibesBackend/src/app.js
+++ caffeVibesBackend/src/app.js
@@ -62,7 +62,6 @@ io.on("connection", (socket) => {
 })
 
 app.use(cors(corsOptions))
-app.options("*", cors(corsOptions))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
